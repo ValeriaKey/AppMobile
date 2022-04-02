420ae2c09bee6b9752dba237f3a2077052695f06
ValeriaKey <valeriakolossova@outlook.com>
2022-04-02 15:33:44 +0300

    Tic Tac Toe

--- AppMobile/AppMobile/Tic-Tac-Toe.xaml.cs
+++ AppMobile/AppMobile/Tic-Tac-Toe.xaml.cs
@@ -10,275 +10,257 @@ using Xamarin.Forms.Xaml;
 namespace AppMobile
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
+
     public partial class Tic_Tac_Toe : ContentPage
+
+
     {
-        string[,] tictac_board = new string[3, 3];
-        bool gameOver = false;
-        bool crossesWon = false;
-        bool noughtsWon = false;
-        public const int EOF = -1;
-        public const int ONE_PLAYER = 1;
-        public const int TWO_PLAYERS = 2;
-        int play_mode = TWO_PLAYERS;
-        bool play_easy = true;
-        int which_player = ONE_PLAYER; // player 1 - starts first.
-        string which_symbol = " ";
-        public const int ROWS = 3;
-        public const int COLS = 3;
-        public const string CROSSES = "X";
-        public const string NOUGHTS = "O";
-        string whichsymbol = " ";
+        Label headingLbl;
+        Grid gameBoard,page_grid;
+        Button newGame, choosePlayer, gameRules;
+        BoxView box;
+        int[,] T = new int[3, 3];
+        bool first = false;
+        bool result;
+        public int win;
 
-        public void OnResetButtonClicked(object sender, EventArgs e)
+
+        public Tic_Tac_Toe()
         {
-            InitilizeBoard();
-            gameOver = false;
-            crossesWon = false;
-            noughtsWon = false;
-        }
 
-        public void InitilizeBoard()
-        {
-            tictac_board[0, 0] = " ";
-            tictac_board[0, 1] = " ";
-            tictac_board[0, 2] = " ";
-            tictac_board[1, 0] = " ";
-            tictac_board[1, 1] = " ";
-            tictac_board[1, 2] = " ";
-            tictac_board[2, 0] = " ";
-            tictac_board[2, 1] = " ";
-            tictac_board[2, 2] = " ";
-            foreach(var theView in gameGrid.Children)
+            // DEFAULT GRID
+
+            page_grid = new Grid
             {
-                int row = 0, column = 0;
-                Button theButton = theView as Button; // cast View to Button
-                if(theButton == ticButtonUpperLeft)
+                RowDefinitions =
                 {
-                    row = 0;
-                    column = 0;
-                }
-                if (theButton == ticButtonUpperMiddle)
+                    new RowDefinition{Height=new GridLength(3,GridUnitType.Star)},
+                    new RowDefinition{Height=new GridLength(1,GridUnitType.Star)},
+                    new RowDefinition{Height=new GridLength(1,GridUnitType.Star)}
+                },
+                ColumnDefinitions =
                 {
-                    row = 0;
-                    column = 1;
+                    new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)},
                 }
-                if (theButton == ticButtonUpperRight)
-                {
-                    row = 0;
-                    column = 2;
-                }
-                if (theButton == ticButtonCenterLeft)
-                {
-                    row = 1;
-                    column = 0;
-                }
-                if (theButton == ticButtonCenterMiddle)
-                {
-                    row = 1;
-                    column = 1;
-                }
-                if (theButton == ticButtonCenterRight)
-                {
-                    row = 1;
-                    column = 2;
-                }
-                if (theButton == ticButtonBottomLeft)
-                {
-                    row = 2;
-                    column = 0;
-                }
-                if (theButton == ticButtonBottomMiddle)
-                {
-                    row = 2;
-                    column = 1;
-                }
-                if (theButton == ticButtonBottomRight)
-                {
-                    row = 2;
-                    column = 2;
-                }
-                SetButtonText(theButton, row, column);
-            }
-            ticButtonUpperLeft.Clicked += OnTicButtonUpperLeftClicked;
-            ticButtonUpperMiddle.Clicked += OnTicButtonUpperMiddleClicked;
-            ticButtonUpperRight.Clicked += OnTicButtonUpperRightClicked;
-            
-            ticButtonCenterLeft.Clicked += OnTicButtonCenterLeftClicked;
-            ticButtonCenterLeft.Clicked -= OnTicButtonCenterLeftClicked;
-
-            ticButtonCenterMiddle.Clicked += OnTicButtonCenterMiddleClicked;
-            ticButtonCenterRight.Clicked += OnTicButtonCenterRightClicked;
-
-            ticButtonBottomLeft.Clicked += OnTicButtonBottomLeftClicked;
-            ticButtonBottomMiddle.Clicked += OnTicButtonBottomMiddleClicked;
-
-            ticButtonBottomRight.Clicked += OnTicButtonBottomRightClicked;
-            // ticButtonBottomRight.Clicked -= OnTicButtonBottomRightClicked;
-
-            resetGame.Clicked += OneResetButtonClicked;
-
-            Content = new StackLayout
-            {
-                Children = {
-                    new Label { Text = "GameBoard Page" },
-                    returnButton,
-                    gameGrid,
-                    playModeLabel,
-                    playerModeSwitch,
-                    resetGame,
-                    difficultPlayLabel,
-                    difficultModeSwitch
-                }   
             };
-            returnButton.Clicked += OneReturnButtonClicked;
-            playerModeSwitch.Toggled += OnPlayerModeSwitchToggle;
-            difficultModeSwitch.Toggled += OnHardPlaySwitchToggle;
-        }
+            // HEADING
 
-        void OnTicButtonBottomRightClicked(object sender, EventArgs e)
-        {
-            int square = 9;
-            GameButtonPressedLogic(sender, square);
-        }
-
-        void OnTicButtonBottomMiddleClicked(object sender, EventArgs e)
-        {
-            int square = 8;
-            GameButtonPressedLogic(sender, square);
-        }
-
-        void OnTicButtonBottomLeftClicked(object sender, EventArgs e)
-        {
-            int square = 7;
-            GameButtonPressedLogic(sender, square);
-        }
-
-        void OnTicButtonCenterRightClicked(object sender, EventArgs e)
-        {
-            int square = 6;
-            GameButtonPressedLogic(sender, square);
-        }
-
-        void OnTicButtonCenterMiddleClicked(object sender, EventArgs e)
-        {
-            int square = 5;
-            GameButtonPressedLogic(sender, square);
-        }
-
-        void OnTicButtonCenterLeftClicked(object sender, EventArgs e)
-        {
-            int square = 4;
-            GameButtonPressedLogic(sender, square);
-        }
-
-        void OnTicButtonUpperRightClicked(object sender, EventArgs e)
-        {
-            int square = 3;
-            GameButtonPressedLogic(sender, square);
-        }
-
-        void OnTicButtonUpperMiddleClicked(object sender, EventArgs e)
-        {
-            int square = 2;
-            GameButtonPressedLogic(sender, square);
-        }
-
-        void OnTicButtonUpperLeftClicked(object sender, EventArgs e)
-        {
-            int square = 1;
-            GameButtonPressedLogic(sender, square);
-        }
-
-        void OnPlayerModeSwitchToggle(object sender, ToggledEventArgs e)
-        {
-            if(e.Value == true)
+            headingLbl = new Label
             {
-                play_mode = ONE_PLAYER;
-            } else
+                Text = "Tic Tac Toe - Game",
+                HorizontalOptions = LayoutOptions.Center,
+                TextColor = Color.Black,
+                TextDecorations = TextDecorations.Underline,
+                FontSize = 20
+            };
+
+            // ** BUTTON **
+            
+            // NEW GAME
+            newGame = new Button
             {
-                play_mode = TWO_PLAYERS;
+                Text = "New Game",
+                BackgroundColor = Color.WhiteSmoke,
+                TextColor = Color.Black
+            };
+            newGame.Clicked += newGame_Clicked;
+            // CONTROL / CHECK
+
+            int gameCheck()
+            {
+                if (T[0, 0] == 1 && T[1, 0] == 1 && T[2, 0] == 1 || T[0, 1] == 1 && T[1, 1] == 1 && T[2, 1] == 1 || T[0, 2] == 1 && T[1, 2] == 1 && T[2, 2] == 1)
+                {
+                    win = 1;
+                }
+                else if (T[0, 0] == 1 && T[0, 1] == 1 && T[0, 2] == 1 || T[1, 0] == 1 && T[1, 1] == 1 && T[1, 2] == 1 || T[2, 0] == 1 && T[2, 1] == 1 && T[2, 2] == 1)
+                {
+                    win = 1;
+                }
+                else if (T[0, 0] == 1 && T[1, 1] == 1 && T[2, 2] == 1 || T[0, 2] == 1 && T[1, 1] == 1 && T[2, 0] == 1)
+                {
+                    win = 1;
+                }
+                else if (T[0, 0] == 2 && T[1, 0] == 2 && T[2, 0] == 2 || T[0, 1] == 2 && T[1, 1] == 2 && T[2, 1] == 2 || T[0, 2] == 2 && T[1, 2] == 2 && T[2, 2] == 2)
+                {
+                    win = 2;
+                }
+                else if (T[0, 0] == 2 && T[0, 1] == 2 && T[0, 2] == 2 || T[1, 0] == 2 && T[1, 1] == 2 && T[1, 2] == 2 || T[2, 0] == 2 && T[2, 1] == 2 && T[2, 2] == 2)
+                {
+                    win = 2;
+                }
+                else if (T[0, 0] == 2 && T[1, 1] == 2 && T[2, 2] == 2 || T[0, 2] == 2 && T[1, 1] == 2 && T[2, 0] == 2)
+                {
+                    win = 2;
+                }
+                else if (T[0, 0] != 0 && T[1, 0] != 0 && T[2, 0] != 0 && T[0, 1] != 0 && T[1, 1] != 0 && T[2, 1] != 0 && T[0, 2] != 0 && T[1, 2] != 0 && T[2, 2] != 0)
+                {
+                    win = 3;
+                }
+
+                return win;
             }
+            // ALERTS 1/2 WIN, TIE
+
+            async void gameOver()
+            {
+                if (win == 1)
+                {
+                    result = await DisplayAlert("BLUE(2) player has won!", "Try again?", "Yes", "No");
+
+                    if (result)
+                    {
+                        makeGameBoard();
+                    }
+                    else
+                    {
+                        System.Environment.Exit(0);
+                    }
+
+                }
+                else if (win == 2)
+                {
+                    result = await DisplayAlert("RED(1) player has won!", "Try again?", "Yes", "No");
+
+                    if (result)
+                    {
+                        makeGameBoard();
+                    }
+                    else
+                    {
+                        System.Environment.Exit(0);
+                    }
+
+                }
+                else if (win == 3)
+                {
+                    result = await DisplayAlert("Friendship has won!", "Try again?", "Yes", "No");
+
+                    if (result)
+                    {
+                        makeGameBoard();
+                    }
+                    else
+                    {
+                        System.Environment.Exit(0);
+                    }
+                }
+            }
+            // ** BUTTON **
+            // CHOOSE PLAYER
+
+            choosePlayer = new Button
+            {
+                Text = "Choose first player",
+                BackgroundColor = Color.WhiteSmoke,
+                TextColor = Color.Black
+            };
+            choosePlayer.Clicked += choosePlayer_Clicked;
+             void newGame_Clicked(object sender, EventArgs e)
+            {
+                makeGameBoard();
+            }
+
+            void choosePlayer_Clicked(object sender, EventArgs e)
+            {
+                choosePlayerQ();
+            }
+
+            async void choosePlayerQ()
+            {
+                string result = await DisplayPromptAsync("Who is FIRST?", "1 - RED, 2 - BLUE", maxLength: 1, keyboard: Keyboard.Numeric);
+
+                if (result == "2")
+                {
+                    first = true;
+                    makeGameBoard();
+                }
+                else
+                {
+                    first = false;
+                    makeGameBoard();
+                }
+            }
+            // BOARD
+            void makeGameBoard()
+            {
+                win = 0;
+                gameBoard = new Grid();
+                for (int i = 0; i < 3; i++)
+                {
+                    gameBoard.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                    gameBoard.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                };
+
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        box = new BoxView { Color = Color.DarkGray, WidthRequest = 200, HeightRequest = 200 };
+                        gameBoard.Children.Add(box, j, i);
+                        TapGestureRecognizer tap = new TapGestureRecognizer();
+                        tap.Tapped += Tap_Tapped; ;
+                        box.GestureRecognizers.Add(tap);
+                    }
+                }
+
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        T[i, j] = 0;
+                    }
+                }
+                page_grid.Children.Add(gameBoard);
+            }
+            
+            
+            
+
+            // CHANGE COLOR WHEN CLICK ON BOXVIEW 1 - RED, 2 - BLUE
+
+            async void Tap_Tapped(object sender, EventArgs e)
+            {
+                var box = (BoxView)sender;
+                var row = Grid.GetRow(box);
+                var col = Grid.GetColumn(box);
+
+                if (first)
+                {
+                    first = false;
+                    box.Color = Color.Blue;
+                    T[row, col] = 1;
+                }
+                else
+                {
+                    first = true;
+                    box.Color = Color.Red;
+                    T[row, col] = 2;
+                }
+
+                gameCheck();
+                gameOver();
+            }
+            // ** BUTTON **
+            // RULES
+            gameRules = new Button
+            {
+                Text = "Rules",
+                BackgroundColor = Color.WhiteSmoke,
+                TextColor = Color.Black
+            };
+            gameRules.Clicked += gameRules_Clicked;
+          
+
+            StackLayout st = new StackLayout
+            { Children = { headingLbl, page_grid, newGame, choosePlayer, gameRules } };
+            Content = st;
         }
 
-        // Change how many players are playing
 
-        Label playModeLabel = new Label
-        {
-            Text = "Press Sitch On for Single Player Mode"
-        };
-        Switch playerModeSwitch = new Switch
-        {
-            HorizontalOptions = LayoutOptions.Start,
-            VerticalOptions = LayoutOptions.Center
-        };
-        Label difficultPlayLabel = new Label
-        {
-            Text = "Press Switch On for Difficult Mode"
-        };
-        Switch difficultModeSwitch = new Switch
-        {
-            HorizontalOptions = LayoutOptions.Start,
-            VerticalOptions = LayoutOptions.Center
-        };
-        Button returnButton = new Button
-        {
-            Text = "Return to Top"
-        };
-        Button ticButtonUpperLeft = new Button
-        {
-            Text = "UL"
-        };
-        Button ticButtonUpperMiddle = new Button
-        {
-            Text = "UM"
-        };
-        Button ticButtonUpperRight = new Button
-        {
-            Text = "UR"
-        };
-        Button ticButtonCenterLeft = new Button
-        {
-            Text = "CL"
-        };
-        Button ticButtonCenterMiddle = new Button
-        {
-            Text = "CM"
-        };
-        Button ticButtonCenterRight = new Button
-        {
-            Text = "CR"
-        };
-        Button ticButtonBottomLeft = new Button
-        {
-            Text = "BL"
-        };
-        Button ticButtonBottomMiddle = new Button
-        {
-            Text = "BM"
-        };
-        Button ticButtonBottomRight = new Button
-        {
-            Text = "BR"
-        };
-        Button resetGame = new Button
-        {
-            Text = "Reset Game"
-        };
-        Grid gameGrid = new Grid
-        {
-            RowDefinitions =
-            {
-                new RowDefinition { Height = GridLength.Auto},
-                new RowDefinition { Height = GridLength.Auto},
-                new RowDefinition { Height = GridLength.Auto}
-            },
-            ColumnDefinitions =
-            {
-                new ColumnDefinition { Width = GridLength.Auto },
-                new ColumnDefinition { Width = GridLength.Auto },
-                new ColumnDefinition { Width = GridLength.Auto }
-            }
-        };
 
+        private void gameRules_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Tic_Tac_Toe_Rules());
+        }
     }
 }
