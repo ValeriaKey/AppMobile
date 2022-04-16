b7a2cd42f1a0db3088fb4c60bab035102e6c0f46
ValeriaKey <valeriakolossova@outlook.com>
2022-04-16 14:23:15 +0300

    Fixes on Browser Page

--- AppMobile/AppMobile/Browser_Page.xaml.cs
+++ AppMobile/AppMobile/Browser_Page.xaml.cs
@@ -12,24 +12,44 @@ namespace AppMobile
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Browser_Page : ContentPage
     {
-        ImageButton homeBtn, backBtn;
-        Entry url;
+        Picker picker;
         WebView webView;
         StackLayout st;
-        public List<String> urls = new List<string> { };
+        Grid gr;
+        Entry url;
+        ImageButton homeBtn, backBtn, favoriteBtn;
+        List<string> urls = new List<string> { "https://tahvel.edu.ee/", "https://moodle.edu.ee/", "https://www.tthk.ee/" };
+
         public Browser_Page()
         {
-            // Buttons 
-            backBtn = new ImageButton()
+            Warning();
+
+            picker = new Picker
             {
-                Source = "ba.png",
-                WidthRequest = 40, HeightRequest = 40,
+                Title = "Favorites",
+                BackgroundColor = Color.Lavender,
+                WidthRequest = 300
+            };
+
+            picker.Items.Add("tahvel.edu.ee");
+            picker.Items.Add("moodle.edu.ee");
+            picker.Items.Add("tthk.ee");
+            picker.SelectedIndexChanged += Picker_SelectedIndexChanged;
+
+            // ** BUTTONS **
+            // Favorite
+            favoriteBtn = new ImageButton()
+            {
+                Source = "f.png",
+                Background = null,
+                WidthRequest = 40,
+                HeightRequest = 40,
                 VerticalOptions = LayoutOptions.Start,
                 HorizontalOptions = LayoutOptions.Start,
                 BackgroundColor = Color.Transparent
-
             };
-            backBtn.Clicked += BackBtn_Clicked;
+            favoriteBtn.Clicked += favoriteBtn_Clicked;
+            // Home
             homeBtn = new ImageButton()
             {
                 Source = "hb.png",
@@ -41,17 +61,33 @@ namespace AppMobile
                 BackgroundColor = Color.Transparent
             };
             homeBtn.Clicked += HomeBtn_Clicked;
+            // Back
+            backBtn = new ImageButton
+            {
+                Source = "ba.png",
+                WidthRequest = 40,
+                HeightRequest = 40,
+                VerticalOptions = LayoutOptions.Start,
+                HorizontalOptions = LayoutOptions.Start,
+                BackgroundColor = Color.Transparent
+            };
+            backBtn.Clicked += BackBtn_Clicked;
+
+            webView = new WebView { 
+
+            };
+
             // Entry 
             url = new Entry()
             {
                 WidthRequest = 180,
                 Placeholder = "https://URL",
-                VerticalOptions= LayoutOptions.Start,
-                HorizontalOptions= LayoutOptions.FillAndExpand
+                VerticalOptions = LayoutOptions.Start,
+                HorizontalOptions = LayoutOptions.FillAndExpand
             };
             url.Completed += Url_Completed;
-            //Grid 
-            Grid gr;
+            // ** LAYOUT OF BUTTONS / ENTRY ** 
+            // Grid
             gr = new Grid
             {
                 Margin = new Thickness(0, 5, 0, 0),
@@ -63,102 +99,75 @@ namespace AppMobile
                 {
                     new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
                      new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
-                      new ColumnDefinition { Width = new GridLength(7, GridUnitType.Star) },
+                      new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
+                      new ColumnDefinition { Width = new GridLength(6, GridUnitType.Star) },
                 },
             };
             gr.Children.Add(homeBtn, 0, 0);
             gr.Children.Add(backBtn, 1, 0);
-            gr.Children.Add(url, 2, 0);
-
-            // Webview
-
-            webView = new WebView
+            gr.Children.Add(favoriteBtn, 2, 0);
+            gr.Children.Add(url, 3, 0);
+            st = new StackLayout
             {
-                Source = new UrlWebViewSource { Url = "https://google.com" },
-                VerticalOptions = LayoutOptions.FillAndExpand,
+                Children = { gr, picker }
             };
-            st = new StackLayout { Children = { gr,webView } };
             Content = st;
-            
-
         }
 
-        private void Url_Completed(object sender, EventArgs e)
+        void Warning()
         {
+            DisplayAlert("Warning!", "Firstly, choose URL from 'Favorites'! \nThank you:) \n// Valeria Novak!", "Let's go!");
+        }
 
-            if (webView != null)
+        private void BackBtn_Clicked(object sender, EventArgs e)
+        {
+            if (webView.CanGoBack)
             {
-                st.Children.Remove(webView);
+                webView.GoBack();
             }
-            
-            var text = "https://" + ((Entry)sender).Text;
-
-            webView.Source = new UrlWebViewSource
-            {
-                Url = text,
-            };
-            st.Children.Add(webView);
-            ((Entry)sender).Text = "";
-            urls.Add(text);
-
-          
         }
 
         private void HomeBtn_Clicked(object sender, EventArgs e)
         {
+            webView.Source = new UrlWebViewSource { Url = urls[2] };
+
+        }
+
+        private async void favoriteBtn_Clicked(object sender, EventArgs e)
+        {
+            if (url.Text != null )
+            {
+                urls.Add("https://" + url.Text);
+                picker.Items.Add(url.Text);
+                await DisplayAlert("Favorite Adding", "New favorite URL successfully added", "Okay!");
+            } else
+            {
+                await DisplayAlert("Favorite Adding", "Enter URL, which you wanna add to FAVORITES", "Okay!");
+            }
+        }
+
+        private void Url_Completed(object sender, EventArgs e)
+        {
+            var text = "https://" + ((Entry)sender).Text;
+            webView.Source = text;
+            st.Children.Add(webView);
+            // ((Entry)sender).Text = "";
+        }
+
+        private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
             if (webView != null)
             {
                 st.Children.Remove(webView);
             }
-
-            webView.Source = new UrlWebViewSource
+            webView = new WebView
             {
-                Url = "https://google.com",
+                Source = new UrlWebViewSource { Url = urls[picker.SelectedIndex] },
+                VerticalOptions = LayoutOptions.FillAndExpand
             };
             st.Children.Add(webView);
         }
 
-        void BackBtn_Clicked(object sender, EventArgs e)
-        {
 
-            if (webView != null)
-            {
-                st.Children.Remove(webView);
-            }
-            /* NOT WORKING 
-            if (webView.CanGoBack)
-            {
-                webView.GoBack();
-            } 
-            */
-
-            
-            Int32 length;
-            int currentCount = urls.Count;
-            int notMore = urls.Count;
-            if (urls.Count > 1)
-            {
-                for (int i = currentCount; (currentCount + 1) > notMore; currentCount--)
-                {
-                    int currentWebSite = (currentCount - 1);
-                    int backWebSite = currentCount - 2;
-                    webView.Source = new UrlWebViewSource
-                    {
-                        Url = urls[backWebSite]
-                    };
-                    st.Children.Add(webView);
-                    urls.Remove(urls[backWebSite]);
-                }
-            }
-            else
-            {
-                webView.Source = new UrlWebViewSource
-                {
-                    Url = "https://google.com",
-                };
-                st.Children.Add(webView);
-            }
-            
-        }
     }
 }
